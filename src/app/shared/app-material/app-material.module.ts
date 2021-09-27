250b63223e4d0f3d943f1442f1ef0a830fed0300
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-09-26 21:34:53 -0300

    Add initial setup to material ui

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }

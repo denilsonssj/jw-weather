e3a936e6991573733c6e5fafe6eb586041ae802b
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-09-30 08:44:17 -0300

    Update dependencies and update style to sidebar

--- src/app/core/layouts/dashboard/dashboard.layout.ts
+++ src/app/core/layouts/dashboard/dashboard.layout.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-
 interface IPathPros {
   path: string;
   label: string;
@@ -8,15 +6,15 @@ interface IPathPros {
 }
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'dashboard-layout',
   templateUrl: './dashboard.layout.html',
   styleUrls: ['./dashboard.layout.scss']
 })
 export class DashboardLayout implements OnInit {
 
   links: IPathPros[] = [
-    { path: '/', label: 'Home', icon: 'home' },
-    { path: '/bookmarks', label: 'Bookmarks', icon: 'star' }
+    { path: '/', label: 'Home', icon: 'home_outline' },
+    { path: '/bookmarks', label: 'Bookmarks', icon: 'star_outline' }
   ];
 
   constructor() { }

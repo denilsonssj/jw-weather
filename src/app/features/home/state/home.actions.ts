35167e66c9d9c3e17a53d02528538c9c21df82b2
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2021-09-30 08:56:50 -0300

    Update store actions to home module

--- src/app/features/home/state/home.actions.ts
+++ src/app/features/home/state/home.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { IBookmark } from 'src/app/core/models/Bookmark.model';
 
 interface IChangeTextActionProps {
   query: string;
@@ -16,4 +17,13 @@ export const loadCurrentWeatherSuccess = createAction(
 
 export const loadCurrentWeatherFailed = createAction(
   '[Weather API] Load Current Weather Failed',
+);
+
+export const toggleBookmark = createAction(
+  '[Home] Toggle Bookmark',
+  props<{ entity: IBookmark }>(),
+);
+
+export const clearHomeState = createAction(
+  '[Home] Clear Home State',
 );
